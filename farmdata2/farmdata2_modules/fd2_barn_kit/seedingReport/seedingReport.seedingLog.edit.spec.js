ab7581b50e6d871f6b04dd50b4b4191fea7b7efb
cschnydman <schnydmc@dickinson.edu>
2024-04-19 14:22:31 -0400

    Created the Issue3 branch and created the seedingReport.seedingLog.edit.spec.js

---
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.seedingLog.edit.spec.js
@@ -0,0 +1,21 @@
+describe("Testing default inputs for Labor chart", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-field-kit/seedingInput")
+    })
+    
+    it("Checks header is Labor", () => { 
+    })
+
+    it("checks fields are empty", () => { 
+        
+    })
+
+    it("Checks dropdown values are default", () => { 
+        
+    })
+
+    it("Checks default time unit", () => { 
+        
+    })
+} )
