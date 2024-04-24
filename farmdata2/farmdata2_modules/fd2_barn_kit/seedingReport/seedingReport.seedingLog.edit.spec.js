c55990a2a5ef51a03f2caa4ef11758da3fa113f6
MuellerrAtDickinsonDotedu <muellerr@dickinson.edu>
2024-04-24 16:37:48 -0400

    changed the .click() so that they click on the save and cancel button. Edited the 3rd it so that it clears the comments on that row. co-authored by: Megan <megantriplett10@gmail.com>, Chloe <schnydmc@dickinson.edu>

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.seedingLog.edit.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.seedingLog.edit.spec.js
@@ -8,34 +8,34 @@ describe("Testing cancel and submit button for table element in seeding report",
     })
     
     it("Make an edit, submit, and check that database was changed", () => { 
-        cy.get('[data-cy=r0-edit-button]').click()
+        cy.get('[data-cy=r0-edit-button]').click({force: true})
         cy.get('[data-cy=td-r0c13]').type('edit:)')
         //can't click save button for some reason
-        cy.get('[data-cy=r0-save-button]').click()
+        cy.get('[data-cy=r0-save-button]').click({force: true})
         //still considering how to check values, because confusion in the issue wording
         cy.get('[data-cy=start-date-select]').type('2020-05-05')
         cy.get('[data-cy=end-date-select]').type('2020-05-05')
         cy.get('[data-cy=generate-rpt-btn]').click()
-        cy.get('[data-cy=td-r0c13]').should('have.text', '<p>edit:)</p>')
+        cy.get('[data-cy=td-r0c13]').should('contain.text', 'edit:)')
     })
     it("Make an edit, cancel, and check that tha database didn't change", () => {
-        cy.get('[data-cy=r0-edit-button]').click()
+        cy.get('[data-cy=r0-edit-button]').click({force: true})
         cy.get('[data-cy=td-r0c13]').type('rar muahaha')
-        cy.get('[data-cy=r0-cancel-button]').click()
+        cy.get('[data-cy=r0-cancel-button]').click({force: true})
         //still considering how to check values, because confusion in the issue wording
         cy.get('[data-cy=start-date-select]').type('2020-05-05')
         cy.get('[data-cy=end-date-select]').type('2020-05-05')
         cy.get('[data-cy=generate-rpt-btn]').click()
-        cy.get('[data-cy=td-r0c13]').should('have.text', '<p>edit:)</p>')
+        cy.get('[data-cy=td-r0c13]').should('contain.text', 'edit:)')
     })
     it("Undo change made in the first it", () => {
-        cy.get('[data-cy=r0-edit-button]').click()
-        cy.get('[data-cy=td-r0c13]').type('')
-        cy.get('[data-cy=r0-save-button]').click()
+        cy.get('[data-cy=r0-edit-button]').click({force: true})
+        cy.get('[data-cy=td-r0c13]').type('{selectall}{del}')
+        cy.get('[data-cy=r0-save-button]').click({force: true})
         //still considering how to check values, because confusion in the issue wording
         cy.get('[data-cy=start-date-select]').type('2020-05-05')
         cy.get('[data-cy=end-date-select]').type('2020-05-05')
         cy.get('[data-cy=generate-rpt-btn]').click()
-        cy.get('[data-cy=td-r0c13]').should('have.text', '')
+        cy.get('[data-cy=td-r0c13]').should('have.text', '     ')
     })
 } )
