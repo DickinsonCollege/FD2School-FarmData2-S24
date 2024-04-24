0a1a08bdd609bf9ac3c78998b1bd40424fcb0b5d
MuellerrAtDickinsonDotedu <muellerr@dickinson.edu>
2024-04-24 16:22:06 -0400

    set URL to correct site, set our framework for our 3 its. Currently dosen't work but working on it. co-authored-by: Megan <megantriplett10@gmail.com>, Chloe <schnydmc@dickinson.edu>

--- farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.seedingLog.edit.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/seedingReport/seedingReport.seedingLog.edit.spec.js
@@ -1,21 +1,41 @@
-describe("Testing default inputs for Labor chart", () => {
+describe("Testing cancel and submit button for table element in seeding report", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-field-kit/seedingInput")
+        cy.visit("farm/fd2-barn-kit/seedingReport")
+        cy.get('[data-cy=start-date-select]').type('2020-05-05')
+        cy.get('[data-cy=end-date-select]').type('2020-05-05')
+        cy.get('[data-cy=generate-rpt-btn]').click()
     })
     
-    it("Checks header is Labor", () => { 
+    it("Make an edit, submit, and check that database was changed", () => { 
+        cy.get('[data-cy=r0-edit-button]').click()
+        cy.get('[data-cy=td-r0c13]').type('edit:)')
+        //can't click save button for some reason
+        cy.get('[data-cy=r0-save-button]').click()
+        //still considering how to check values, because confusion in the issue wording
+        cy.get('[data-cy=start-date-select]').type('2020-05-05')
+        cy.get('[data-cy=end-date-select]').type('2020-05-05')
+        cy.get('[data-cy=generate-rpt-btn]').click()
+        cy.get('[data-cy=td-r0c13]').should('have.text', '<p>edit:)</p>')
     })
-
-    it("checks fields are empty", () => { 
-        
+    it("Make an edit, cancel, and check that tha database didn't change", () => {
+        cy.get('[data-cy=r0-edit-button]').click()
+        cy.get('[data-cy=td-r0c13]').type('rar muahaha')
+        cy.get('[data-cy=r0-cancel-button]').click()
+        //still considering how to check values, because confusion in the issue wording
+        cy.get('[data-cy=start-date-select]').type('2020-05-05')
+        cy.get('[data-cy=end-date-select]').type('2020-05-05')
+        cy.get('[data-cy=generate-rpt-btn]').click()
+        cy.get('[data-cy=td-r0c13]').should('have.text', '<p>edit:)</p>')
     })
-
-    it("Checks dropdown values are default", () => { 
-        
-    })
-
-    it("Checks default time unit", () => { 
-        
+    it("Undo change made in the first it", () => {
+        cy.get('[data-cy=r0-edit-button]').click()
+        cy.get('[data-cy=td-r0c13]').type('')
+        cy.get('[data-cy=r0-save-button]').click()
+        //still considering how to check values, because confusion in the issue wording
+        cy.get('[data-cy=start-date-select]').type('2020-05-05')
+        cy.get('[data-cy=end-date-select]').type('2020-05-05')
+        cy.get('[data-cy=generate-rpt-btn]').click()
+        cy.get('[data-cy=td-r0c13]').should('have.text', '')
     })
 } )
