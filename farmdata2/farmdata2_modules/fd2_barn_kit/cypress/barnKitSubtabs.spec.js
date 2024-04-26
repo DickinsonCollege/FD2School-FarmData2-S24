63ad2b92d9b6adf431c5092a0031460d6fddf1e7
QuangPhung15 <phunghuyquang150904@gmail.com>
2024-04-26 14:10:25 -0400

    Finished testing BarnKit subtabs
    co-authored-by: linhkhanhhoang hoangli@dickinson.edu
    co-authored-by: HaNguyen03@dickinson.edu

--- farmdata2/farmdata2_modules/fd2_barn_kit/cypress/barnKitSubtabs.spec.js
+++ farmdata2/farmdata2_modules/fd2_barn_kit/cypress/barnKitSubtabs.spec.js
@@ -4,19 +4,19 @@ describe('Test the sub-tabs of the FieldKit tab', () => {
         cy.visit('/farm/fd2-barn-kit')
     })
 
-    it('Check Info, Seeding Report, and Transplanting Report sub-tabs', () => {
+    it('Check the BarnKit tab contains sub-tabs for “Info”, “Seeding Report” and “Transplanting Report”', () => {
         cy.get('.tabs--secondary').contains('Info').should('exist')
         cy.get('.tabs--secondary').contains('Seeding Report').should('exist')
         cy.get('.tabs--secondary').contains('Transplanting Report').should('exist')
     })
     
-    it('Check that the order of the tabs: Info, Seeding Report, and Transplanting Report', () => {
+    it('Check the order of the tabs is “Info”, “Seeding Report” and then “Transplanting Report”', () => {
         cy.get('.tabs--secondary').children().eq(0).contains('Info')
         cy.get('.tabs--secondary').children().eq(1).contains('Seeding Report')
         cy.get('.tabs--secondary').children().eq(2).contains('Transplanting Report')
     })
     
-    it('Check that there are only 3 sub-tabs', () => {
+    it('Check there are the correct number of sub-tabs (3 at this time)', () => {
         cy.get(".tabs--secondary").children().should('have.length', 3)
     })
 })
