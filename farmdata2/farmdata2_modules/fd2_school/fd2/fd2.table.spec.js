221ca591628085527e0bbfbe50e93da021fc1942
jadoont <tayyabajadoon11@gmail.com>
2024-05-01 16:06:59 -0400

    adds cypress test

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -0,0 +1,12 @@
+
+describe('Harvest Report Table', () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/fd2/fd2.html")
+    });
+  
+    it('should display the harvest report table', () => {
+      
+    });
+  });
+  
