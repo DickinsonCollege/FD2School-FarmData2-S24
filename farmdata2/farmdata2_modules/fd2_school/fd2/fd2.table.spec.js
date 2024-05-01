27e4a4cb36dae81c6399fce94c3be00e9ca634f3
jadoont <tayyabajadoon11@gmail.com>
2024-05-01 16:23:04 -0400

    filter by crop test

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.table.spec.js
@@ -13,6 +13,23 @@ describe('Harvest Report Table', () => {
         cy.get("[data-cy=h2]").should("have.text", "Crop");
         cy.get("[data-cy=h3]").should("have.text", "Quantity");
         cy.get("[data-cy=h4]").should("have.text", "Unit");
+
+        cy.get("[data-cy=header-row]").children().should('have.length', 8);
+
+
+        it('should filter the table by crop correctly', () => {
+        
+            cy.get("[data-cy=generate-report-button]").click();
+        
+           
+            cy.get("[data-cy=crop-dropdown]").click(); 
+            cy.get("[data-cy=crop-dropdown] > .dropdown-input").select("ZUCCHINI"); 
+        
+
+            cy.get("[data-cy=body-row]").each(($row) => {
+              cy.wrap($row).find("[data-cy=crop-column]").should('have.text', 'ZUCCHINI');
+            });
+          });
     });
   });
   
