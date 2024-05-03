af4ee1446422b1ab0cc526983b6e0c9d0e4c045e
evelynpham04 <phamphuonglinh250104@gmail.com>
2024-05-03 00:00:42 -0400

    Add testing for the Harvest Report Generation

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -4,19 +4,10 @@ describe("Test the harvest report default values", () => {
         cy.visit("/farm/fd2-school/e2e");
     });
 
-    it("Check the page header", () => {
-        cy.get("[data-cy=page-header]").should("have.text", "Harvest Report");
-    });
+    it("Check the generate report button", () => {
+        cy.get("[data-cy=report-title]").should("not.exist")
+        cy.get("[data-cy=generate-report]").click()
+        cy.get("[data-cy=report-title]").should("be.visible")
 
-    it("Check the default start and end dates", () => {
-        cy.get("[data-cy=start-date]").should("have.value","2020-05-05")
-        cy.get("[data-cy=end-date]").should("have.value","2020-05-15")
     })
-
-    it("Check the crops in crop list", () => {
-        cy.get("[data-cy=crop-list]").children().eq(0).should("have.text", "ARUGULA");
-        cy.get("[data-cy=crop-list]").children().eq(4).should("have.text", "BEAN-FAVA");
-        cy.get("[data-cy=crop-list]").children().eq(110).should("have.text", "ZUCCHINI");
-        cy.get("[data-cy=crop-list]").children().should("have.length", 111);
-    });
 });
