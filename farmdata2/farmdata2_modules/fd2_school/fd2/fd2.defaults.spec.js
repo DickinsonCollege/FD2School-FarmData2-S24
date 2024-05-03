91c2d50a86b90de8085be49c9b3b92db4277bb1c
evelynpham04 <phamphuonglinh250104@gmail.com>
2024-05-03 00:23:39 -0400

    Add FD2 new sub-tab

---
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.defaults.spec.js
@@ -0,0 +1,16 @@
+describe("Test the harvest report default values", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2");
+        cy.visit("/farm/fd2-school/e2e");
+    });
+
+    it("Check report details", () => {
+        cy.get("[data-cy=farm-name]").should("not.exist")
+        cy.get("[data-cy=language]").should("not.exist")
+        cy.get("[data-cy=user]").should("not.exist")
+        cy.get("[data-cy=generate-report]").click()
+        cy.get("[data-cy=farm-name]").should("have.text","Farm:  Sample Farm")
+        cy.get("[data-cy=language]").should("have.text","en")
+        cy.get("[data-cy=user]").should("have.text","manager1")
+    })
+});
