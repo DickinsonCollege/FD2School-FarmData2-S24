737d0d02812c1e1493cf594f367779ef6d092bb4
evelynpham04 <phamphuonglinh250104@gmail.com>
2024-05-03 00:07:35 -0400

    Add testing for the report details

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.defaults.spec.js
@@ -4,10 +4,13 @@ describe("Test the harvest report default values", () => {
         cy.visit("/farm/fd2-school/e2e");
     });
 
-    it("Check the generate report button", () => {
-        cy.get("[data-cy=report-title]").should("not.exist")
+    it("Check report details", () => {
+        cy.get("[data-cy=farm-name]").should("not.exist")
+        cy.get("[data-cy=language]").should("not.exist")
+        cy.get("[data-cy=user]").should("not.exist")
         cy.get("[data-cy=generate-report]").click()
-        cy.get("[data-cy=report-title]").should("be.visible")
-
+        cy.get("[data-cy=farm-name]").should("have.text","Farm:  Sample Farm")
+        cy.get("[data-cy=language]").should("have.text","en")
+        cy.get("[data-cy=user]").should("have.text","manager1")
     })
 });
