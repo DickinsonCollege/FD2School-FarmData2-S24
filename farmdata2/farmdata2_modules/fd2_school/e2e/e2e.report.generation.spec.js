7c005c39346280b5bbbad014b1d2f955177a08e0
Andrew <laia@DESKTOP-S9MO7FP>
2024-05-04 02:45:05 -0400

    made tests for farm name, user name, language

--- farmdata2/farmdata2_modules/fd2_school/e2e/e2e.report.generation.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/e2e/e2e.report.generation.spec.js
@@ -9,4 +9,12 @@ describe("Test the harvest report generation", () => {
         cy.get("[data-cy=generate-report-button]").click()
         cy.get("[data-cy=report-title]").should("be.visible")
     })
+    it("Test the farm name", () => {
+        cy.get("[data-cy=generate-report-button]").click()
+        cy.get("[data-cy=farm-name]").should("contain.text","Sample Farm")
+        cy.get("[data-cy=user-name]").should("contain.text","manager1")
+        cy.get("[data-cy=language]").should("have.text","English")
+
+
+    })
 })
