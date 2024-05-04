bd41b16f68cd4554f0cf5cb4d9c52e7084bd1901
Andrew <laia@DESKTOP-S9MO7FP>
2024-05-04 17:28:28 -0400

    modified fd2 test file to visit fd2 instead of e2e

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.report.generation.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.report.generation.spec.js
@@ -1,7 +1,7 @@
 describe("Test the harvest report generation", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-school/fd2")
     })
       
     it("Test the generate report button", () => {
