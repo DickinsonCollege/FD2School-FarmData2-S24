75834fdb515cd0d6352177ee3c495ecfcf07e864
Cubin1215 <pranavazad1215@gmail.com>
2024-05-07 16:21:37 -0400

    Updated fd2.defaults.spec.js to test the component

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.defaults.spec.js
@@ -2,22 +2,20 @@ describe("Test the harvest report default values", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-school/FD2")
-        cy.get("[data-cy=page-header]").should("have.text","Harvest Report")
-        cy.get("[data-cy=start-date]").should("have.value","2020-05-05")
-        cy.get("[data-cy=end-date]").should("have.value","2020-05-15")
-        cy.get("[data-cy=crop-dropdown]").children().eq(0).should("contain.text", "ARUGULA");
-        cy.get("[data-cy=crop-dropdown]").children().eq(4).should("contain.text", "BEAN-FAVA");
-        cy.get("[data-cy=crop-dropdown]").children().last().should("contain.text", "ZUCCHINI");
-        cy.get("[data-cy=crop-dropdown]").children().should("have.length", 111);
     })
       
     it("Check the page header", () => {
-    
+        cy.get("[data-cy=page-header]").should("have.text","Harvest Report")
     })
     it("Check the default start and end date", () => {
-
+        cy.get("[data-cy=start-date]").should("have.value","2020-05-05")
+        cy.get("[data-cy=end-date]").should("have.value","2020-05-15")
     })
     it("Check the crop dropdown menu", () => {
+        cy.get("[data-cy=crop-dropdown]> [data-cy=dropdown-input]").children().eq(0).should("contain.text", "All");
+        cy.get("[data-cy=crop-dropdown]> [data-cy=dropdown-input]").children().eq(4).should("contain.text", "BEAN-DRY");
+        cy.get("[data-cy=crop-dropdown]> [data-cy=dropdown-input]").children().last().should("contain.text", "ZUCCHINI");
+        cy.get("[data-cy=crop-dropdown]> [data-cy=dropdown-input]").children().should("have.length", 112);
 
     })
 })
