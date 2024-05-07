6819491b868983c21802ea2822281553ed99e027
Cubin1215 <pranavazad1215@gmail.com>
2024-05-07 15:54:17 -0400

    Updated spec.js files to reach directory FD2

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.defaults.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.defaults.spec.js
@@ -1,7 +1,7 @@
 describe("Test the harvest report default values", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-school/FD2")
         cy.get("[data-cy=page-header]").should("have.text","Harvest Report")
         cy.get("[data-cy=start-date]").should("have.value","2020-05-05")
         cy.get("[data-cy=end-date]").should("have.value","2020-05-15")
--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.user.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.user.spec.js
@@ -1,7 +1,7 @@
 describe("Test the harvest report user output values", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
-        cy.visit("/farm/fd2-school/e2e")
+        cy.visit("/farm/fd2-school/FD2")
         cy.get("[data-cy=header-check]").should("not.exist")
         cy.get("[data-cy=generate-button]").click()
         cy.get("[data-cy=header-check]").should("be.visible");
