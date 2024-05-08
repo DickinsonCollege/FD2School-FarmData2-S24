6c2f493cc07b8db6c7749602355bfcee5c105132
Cubin1215 <pranavazad1215@gmail.com>
2024-05-07 20:54:06 -0400

    Created cypress file for the table component

---
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
@@ -0,0 +1,9 @@
+describe("Test the CustomTableComponent Table", () => {
+    beforeEach(() => {
+        cy.login("manager1", "farmdata2")
+        cy.visit("/farm/fd2-school/FD2")
+    })
+
+    it( () => {
+    })
+})
