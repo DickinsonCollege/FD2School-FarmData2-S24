48f6409d9155b6be3cf73cea6f816f0da9e2789c
Cubin1215 <pranavazad1215@gmail.com>
2024-05-07 21:16:30 -0400

    Coded the tests for the table header

--- farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/FD2/fd2.table.spec.js
@@ -1,9 +1,16 @@
-describe("Test the CustomTableComponent Table", () => {
+describe("Test the Table header", () => {
     beforeEach(() => {
         cy.login("manager1", "farmdata2")
         cy.visit("/farm/fd2-school/FD2")
     })
-
-    it( () => {
+      
+    it("Check the table header", () => {
+        cy.get("[data-cy=generate-button]").click()
+        cy.get("[data-cy=h0]").should("have.text","Index")
+        cy.get("[data-cy=h1]").should("have.text","Date")
+        cy.get("[data-cy=h2]").should("have.text","Area")
+        cy.get("[data-cy=h3]").should("have.text","Crop")
+        cy.get("[data-cy=h4]").should("have.text","Yield")
+        cy.get("[data-cy=h5]").should("have.text","Units")
     })
 })
