b075dd1e7b40cba3b73ba236285974a9cd6be09e
linhkhanhhoang <hoangli@dickinson.edu>
2024-05-09 11:54:10 -0400

    updated beforeEach in spec.js files

--- farmdata2/farmdata2_modules/fd2_school/fd2/fd2.report.spec.js
+++ farmdata2/farmdata2_modules/fd2_school/fd2/fd2.report.spec.js
@@ -2,7 +2,7 @@ describe("Test the harvest report functionality", () => {
 
     beforeEach(() => { 
         cy.login("manager1", "farmdata2") 
-        cy.visit("/farm/fd2-school/e2e") 
+        cy.visit("/farm/fd2-school/fd2") 
     }) 
 
     it("Check Generate Report Button", () => { 
